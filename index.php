1f535e329ff4413dd9a30c3a63b298ee21390bd0
Rizky Kurniawan <contact.rizkykurniawan@gmail.com>
2022-01-26 02:17:32 +0800

    starter.html template breakdown

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!--
+This is a starter template page. Use this page to start your new project from
+scratch. This page gets rid of all links and provides the needed markup only.
+-->
+<html lang="en">
+<?php include "partials/head.php" ?>
+
+<body class="hold-transition sidebar-mini">
+    <div class="wrapper">
+        <?php include "partials/nav.php" ?>
+        <?php include "partials/sidebar.php" ?>
+        <div class="content-wrapper">
+            <?php include "partials/content.php" ?>
+        </div>
+        <?php include "partials/control.php" ?>
+        <?php include "partials/footer.php" ?>
+    </div>
+    <?php include "partials/scripts.php" ?>
+</body>
+
+</html>
